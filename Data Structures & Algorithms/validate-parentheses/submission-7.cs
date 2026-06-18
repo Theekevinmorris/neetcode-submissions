cf868e5ab8550afcf2cf8fbbd229cfcd8b3a2523
Theekevinmorris <71162449+Theekevinmorris@users.noreply.github.com>
2026-06-18 14:14:01 -0700

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsValid(string s) {
+        if(s.Length % 2  > 0)
+            return false;
+        var stack = new Stack<char>();
+        foreach(char c in s){
+            if (c == '{' || c == '(' || c == '['){
+                stack.Push(c);
+            }else{
+                if(stack.Count == 0) return false;
+                
+                if(c == '}' && stack.Peek() == '{'){
+                    stack.Pop();
+                }else if(c == ']' && stack.Peek() == '['){
+                    stack.Pop();
+                }else if(c == ')' && stack.Peek() == '('){
+                    stack.Pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        return stack.Count == 0;
+    }
+}
