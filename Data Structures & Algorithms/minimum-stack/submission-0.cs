6d1e3335abc1846ed93b8052dc6fc864af317845
Theekevinmorris <71162449+Theekevinmorris@users.noreply.github.com>
2026-06-18 14:30:39 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cs
@@ -0,0 +1,29 @@
+public class MinStack {
+
+    private Stack<int> _stack = new Stack<int>();
+    private Stack<int> _minStack = new Stack<int>();
+    private int currentMin = int.MaxValue;
+
+    public MinStack() {
+        
+    }
+    
+    public void Push(int val) {
+        _stack.Push(val);
+        _minStack.Push(Math.Min(currentMin,val));
+        currentMin = _minStack.Peek();
+    }
+    
+    public void Pop() {
+        _stack.Pop();
+        _minStack.Pop();
+    }
+    
+    public int Top() {
+      return _stack.Peek();
+    }
+    
+    public int GetMin() {
+        return _minStack.Peek();
+    }
+}
