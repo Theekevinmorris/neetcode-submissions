ee83efd1c33cb4924b82df8aba5b36bc362e47c9
Theekevinmorris <71162449+Theekevinmorris@users.noreply.github.com>
2026-06-21 09:59:24 -0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LastStoneWeight(int[] stones) {
+        if (stones.Length == 1)
+            return stones[0];
+
+        var items = stones.Select(n => (n, -n));
+        PriorityQueue<int,int> pq = new PriorityQueue<int,int>(items);
+
+        while(pq.Count > 1){
+            int largest = pq.Dequeue();
+            int secondLargest = pq.Dequeue();
+
+            if(largest > secondLargest){
+                int newValue = largest - secondLargest;
+                pq.Enqueue(newValue, -newValue);
+            }
+        }
+
+        if(pq.Count == 1){
+            return pq.Peek();
+        } 
+
+        return 0;
+
+
+    }
+}
