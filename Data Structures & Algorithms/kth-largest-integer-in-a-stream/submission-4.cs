8aa4181e15a0131f04b9da3c780ed03ca5270079
Theekevinmorris <71162449+Theekevinmorris@users.noreply.github.com>
2026-06-21 09:46:18 -0700

    Add: kth-largest-integer-in-a-stream - submission-4

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-4.cs
@@ -0,0 +1,27 @@
+public class KthLargest {
+
+    private PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+    private int _k;
+
+    public KthLargest(int k, int[] nums) {
+        _k = k;
+        
+        foreach(int n in nums){
+            pq.Enqueue(n,n);
+        }
+
+        while(pq.Count > _k){
+            pq.Dequeue();
+        }
+    }
+    
+    public int Add(int val) {
+        pq.Enqueue(val,val);
+
+        if (pq.Count > _k){
+            pq.Dequeue();
+        }
+
+        return pq.Peek();
+    }
+}
