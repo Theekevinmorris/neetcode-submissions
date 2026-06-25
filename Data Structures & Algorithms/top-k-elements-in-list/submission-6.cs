5437b7f762058fef3066dd7f6e9134d644c6a560
Theekevinmorris <71162449+Theekevinmorris@users.noreply.github.com>
2026-06-25 10:55:16 -0700

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        var dict = new Dictionary<int,int>();
+        var pqueue = new PriorityQueue<int,int>();
+
+
+        for(int i = 0; i < nums.Length; i++){
+            if(dict.ContainsKey(nums[i])){
+                dict[nums[i]]++;
+            }else{
+                dict[nums[i]] = 1;
+            }
+        }
+
+        foreach(var j in dict){
+            if (pqueue.Count < k){
+                pqueue.Enqueue(j.Key, j.Value);
+            }else{
+                pqueue.TryPeek(out int element, out int priority);
+                if (priority < j.Value){
+                    pqueue.Dequeue();
+                    pqueue.Enqueue(j.Key, j.Value);
+                }
+            }
+        }
+
+        var result = new List<int>();
+        while (pqueue.Count > 0)
+        {
+            result.Add(pqueue.Dequeue());
+        }
+        return result.ToArray();
+
+    }
+}
